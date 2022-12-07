c941858ff8e97e5d200ad9c504d3ce9ddd782853
jmartint13 <joelmt04yt@gmail.com>
2022-12-07 17:00:47 +0100

    First Commit

---
+++ aplication.py
@@ -0,0 +1,11 @@
+menu = "1. Introdueix una frase \n2. Elimina les vocals \n3. Elimina les consonants " \
+       "\n4. Ordena la frase al revés i printala per pantalla " \
+       "\n5. Ordena les paraules de la frase en orde ascendent i printales per pantalla"
+
+
+
+
+
+
+
+
