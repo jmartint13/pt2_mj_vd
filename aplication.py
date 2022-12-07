19833f2a209de96bc39770064d52dabc34a86d5a
jmartint13 <joelmt04yt@gmail.com>
2022-12-07 17:50:01 +0100

    Parte alumno1

--- aplication.py
+++ aplication.py
@@ -3,6 +3,24 @@ menu = "1. Introdueix una frase \n2. Elimina les vocals \n3. Elimina les consona
        "\n5. Ordena les paraules de la frase en orde ascendent i printales per pantalla"
 
 
+def insertaFrase():
+       frase = input("Inserta una frase")
+       return frase
+
+def eliminaVocales(frase):
+       vocales = "aeiou"
+       for letra in frase:
+              if letra.lower() == vocales:
+                     frase -= frase[letra]
+       return frase
+
+def ordenaReves(frase):
+       nFrase = ""
+       for letra in range(0, len(frase), -1):
+              nFrase += frase[letra]
+
+
+
 
 
 
